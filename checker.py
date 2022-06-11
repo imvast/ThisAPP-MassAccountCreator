3b4edb29a842e937fe9c975d81ce60225333b9bc
vast <60797067+imvast@users.noreply.github.com>
2022-06-10 21:56:17 -0400

    quick fix

--- checker.py
+++ checker.py
@@ -2,27 +2,31 @@ import threading, random, requests
 
 
 def check():
-    _username = ''.join(random.choices('abcdefghijklmnopqrstuvwxyz', k=3))
+    _username = ''.join(random.choices('abcdefghijklmnopqrstuvwxyz', k=4))
     _check = requests.post(
         "https://api.ws.thisapp.so/graphql",
-        json = {
-               "operationName": "CheckUsername",
-               "variables": {
-                 "input": {
-                   "username": _username
-                 }
-               },
-               "query": "query CheckUsername($input: CheckUsernameInput!) {\n  checkUsername(input: $input) {\n    isAvailable\n    __typename\n  }\n}\n"
+        json={
+            "operationName": "CheckUsername",
+            "variables": {
+                "input": {
+                    "username": _username
+                }
             },
-        headers = {
-                "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.115 Safari/537.36",
-                "origin": "https://thisapp.com"
-            }
+            "query": "query CheckUsername($input: CheckUsernameInput!) {\n  checkUsername(input: $input) {\n    isAvailable\n    __typename\n  }\n}\n"
+        },
+        headers={
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.115 Safari/537.36",
+            "origin": "https://thisapp.com"
+        }
     )
-    if '"isAvailable":true' in _check.text:
-        print(_username, file=open('available.txt'))
+    with open('available.txt','a') as _available:
+        if '"isAvailable":true' in _check.text:
+            _available.write('%s\n'%(str(_username)))
+            print(_username)
+        
+
 
 
 while True:
-    if threading.active_count() < 500:
+    if threading.active_count() < 100:
         threading.Thread(target=check).start()
